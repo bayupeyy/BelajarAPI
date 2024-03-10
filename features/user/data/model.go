0aaa0d63e1cde3fe24b9cc29c94df5ac3b91e62b
bayupeyy <mochamadbayuajie@gmail.com>
2024-03-11 02:20:22 +0700

    Chore: Update Fitur DB

--- features/user/data/model.go
+++ features/user/data/model.go
@@ -6,5 +6,5 @@ type User struct {
 	Nama       string
 	Hp         string          `gorm:"type:varchar(13);uniqueIndex;primaryKey" json:"hp" form:"hp" validate:"required,max=13,min=10"`
 	Password   string          `json:"password" form:"password" validate:"required"`
-	Activities []data.Activity `gorm:"foreignKey:UserID;references:Hp"`
+	Activities []data.Activity `gorm:"foreignKey:Pemilik;references:Hp"`
 }
